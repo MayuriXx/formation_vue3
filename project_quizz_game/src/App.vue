ca544e5f62e16f321f70cf92b2bb59516971e888
Martho Evan <martho.evan@gmail.com>
2024-03-05 16:07:22 +0100

    feat: clean projects

--- project_quizz_game/src/App.vue
+++ project_quizz_game/src/App.vue
@@ -1,17 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="My first VueJS App"/> -->
+  <div>
+    <h1>Quizz Game</h1>
+  </div>
+  <input type="radio" name="options" value="True">
+  <label>True</label><br>
+  <input type="radio" name="options" value="False">
+  <label>False</label><br>
+  <button class="send" type="button">Submit</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // }
 }
+
+//https://opentdb.com/api.php?amount=1&category=21 -> API
 </script>
 
 <style lang="scss">
@@ -21,6 +31,21 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+  
+  input[type="radio"] {
+    margin: 10px;
+  }
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
 }
 </style>
