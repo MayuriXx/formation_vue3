4c06bb5bf14822b3340a29a229d4dd79697b9ccc
Martho Evan <martho.evan@gmail.com>
2024-03-05 16:21:53 +0100

    feat: add axios

--- project_quizz_game/src/main.js
+++ project_quizz_game/src/main.js
@@ -1,4 +1,6 @@
+import axios from 'axios'
 import { createApp } from 'vue'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).mount('#app').use(VueAxios, axios)
